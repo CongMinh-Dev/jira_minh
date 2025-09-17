3e98798060c4e61e43e88bb1bca215a2a3aa5ae3
Minh <lightstormlxag93@gmail.com>
2025-09-18 00:29:07 +0700

    up

--- src/services/config.js
+++ src/services/config.js
@@ -4,7 +4,7 @@ export const http = axios.create({
   baseURL: "https://jiranew.cybersoft.edu.vn/api/",
   headers: {
     tolenCybersoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAxMCIsIkhldEhhblN0cmluZyI6IjAxLzA5LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcyNTE0ODgwMDAwMCIsIm5iZiI6MTY5ODY4NTIwMCwiZXhwIjoxNzI1Mjk2NDAwfQ.CPY1b9IiMcklQZ9hjqIzrdiOlQ5YnV4VpzGu_yZr7G0",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAxMCIsIkhldEhhblN0cmluZyI6IjAxLzA5LzIwMjciLCJIZXRIYW5UaW1lIjoiMTcyNTE0ODgwMDAwMCIsImlhdCI6MTc1ODEyOTU4NCwiZXhwIjoxNzU4NTYxNTg0fQ.-U4gFwz1HM8yWlR_6oz8xwUdOgSkNq3ZbCHgxTP2lUE",
   },
   timeout: 30000,
 });
